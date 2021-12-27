dec3923fa89a8f2f89859ab47dce6a61c33d2999
peanut996 <849421294godw@gmail.com>
2021-12-27 19:15:29 +0800

    download all search page

--- src/qunar.py
+++ src/qunar.py
@@ -1,15 +1,25 @@
+import os.path
+import random
+
+import requests as requests
+import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import lxml
 
-QUNAR_URL = 'http://travel.qunar.com/travelbook/list/22-shanghai-299878/hot_ctime/1.htm'
+QUNAR_URL_TEMPLATE = 'http://travel.qunar.com/travelbook/list/22-shanghai-299878/hot_ctime/{}.htm'
+
+def download_all_search_page():
+    if not os.path.exists("../html/qunar/search"):
+        os.makedirs("../html/qunar/search")
+    for i in range(1, 189):
+        url = QUNAR_URL_TEMPLATE.format(i)
+        time.sleep(random.randint(1, 3))
+        r = requests.get(url)
+        with open("../html/qunar/search/{}.html".format(i), 'w') as f:
+            f.write(r.text)
+        print("page {} done".format(i))
 
 
 if __name__ == '__main__':
-
-    driver = webdriver.Edge()
-    print("等待页面加载完成")
-    driver.get(QUNAR_URL)
-    print("页面加载完成")
-
-    page = BeautifulSoup(driver.page_source, 'lxml')
+    download_all_search_page()
