50ab578c40fb714fd244da3595259cd14ed89ae1
peanut996 <849421294godw@gmail.com>
2021-12-30 22:55:13 +0800

    update pool

--- src/ctrip.py
+++ src/ctrip.py
@@ -18,7 +18,7 @@ def download_ctrip_search_page(page_num):
     while retry_count < 20:
         proxy = get_proxy()
         try:
-            time.sleep(random.randint(1, 10))
+
             res = requests.get(url, headers=headers, timeout=(3, 7))
             if res.status_code != 200:
                 raise Exception('status code is {}'.format(res.status_code))
@@ -27,21 +27,23 @@ def download_ctrip_search_page(page_num):
             break
         except Exception as e:
             delete_proxy(proxy)
+            time.sleep(random.randint(1, 2))
             retry_count += 1
             print('download page {} failed, retry count: {}, reason: {}'.format(page_num, retry_count, str(e)))
 
 
 def save_search_page(page_num, html):
     print('saving page {}...'.format(page_num))
-    with open("../html/ctrip/search/page{}.html".format(page_num), 'w', encoding='utf-8') as f:
+    with open("../html/ctrip/search/{}.html".format(page_num), 'w', encoding='utf-8') as f:
         f.write(html)
 
 def download_all_search_page():
     if not os.path.exists("../html/ctrip/search"):
         os.makedirs("../html/ctrip/search")
-    pool = ThreadPoolExecutor(max_workers=32)
+    pool = ThreadPoolExecutor(max_workers=5)
     workers = []
-    for i in range(1, 11):
+    for i in range(1, 190):
+        time.sleep(random.randint(1, 2))
         workers.append(pool.submit(download_ctrip_search_page, i))
 
     for worker in as_completed(workers):
