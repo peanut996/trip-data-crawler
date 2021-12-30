8430fa1aecb0231b96184b7d19cade9584c4532b
peanut996 <peanut996@qq.com>
2021-12-30 11:24:02 +0800

    test selenium for manjaro

--- src/ctrip.py
+++ src/ctrip.py
@@ -1,1 +1,7 @@
-print("hello world")
+from selenium import webdriver
+from tool import *
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
+
+if __name__ == '__main__':
+    print("hello world")
