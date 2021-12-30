6f87ac30d3ac10e438555298eef9f8f68b9ca77a
peanut996 <849421294godw@gmail.com>
2021-12-30 22:27:53 +0800

    download search page

--- src/ctrip.py
+++ src/ctrip.py
@@ -16,22 +16,25 @@ def download_ctrip_search_page(page_num):
     while retry_count < 20:
         # proxy = get_proxy()
         try:
-            time.sleep(random.randint(1,2))
-            res = requests.get(url, headers=headers,timeout=(3,7))
+            time.sleep(random.randint(1, 2))
+            res = requests.get(url, headers=headers, timeout=(3, 7))
             if res.status_code != 200:
                 raise Exception('status code is {}'.format(res.status_code))
             print('page {} downloaded'.format(page_num))
             save_search_page(page_num, res.text)
+            break
         except Exception as e:
             # delete_proxy(proxy)
             retry_count += 1
             time.sleep(5)
             print('download page {} failed, retry count: {}, reason: {}'.format(page_num, retry_count + 1, str(e)))
 
+
 def save_search_page(page_num, html):
     print('saving page {}...'.format(page_num))
     with open("../html/ctrip/search/page{}.html".format(page_num), 'w', encoding='utf-8') as f:
         f.write(html)
+
 
 if __name__ == '__main__':
     if not os.path.exists("../html/ctrip/search"):
